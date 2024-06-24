ae60967ad6bda48013187e4614a292a2cbe38dd0
Sasha Richardson <sasha_richardson@hotmail.co.uk>
2024-06-24 19:47:18 +0100

    Meta

--- app.vue
+++ app.vue
@@ -1,3 +1,11 @@
+<script setup lang="ts">
+  useHead({
+    title: 'Vizune Template: Kantai',
+    meta: [
+      { name: 'description', content: 'Nuxt template called Kantai by Vizune.com' }
+    ]
+  })
+</script>
 <template>
   <div>
     <AppHeader />
