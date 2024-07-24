ae4861cd818ff14e8a275a5450ead9ed3048efaa
Sasha Richardson <sasha_richardson@hotmail.co.uk>
2024-07-24 11:04:39 +0100

    Adjust navigation spacing

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="w-full pt-6 sm:pt-12 pb-1 bg-black text-center">
     <SiteName />
-    <Navigation class="mt-12" />
+    <Navigation class="pt-6 lg:pt-12" />
   </div>
 </template>
